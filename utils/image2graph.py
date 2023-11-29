3f4c63a5513726f03e4b56210ddf91e1892793c4
gauravsh0812 <gauravs@arizona.edu>
2023-11-29 11:01:58 -0700

    updating

--- utils/image2graph.py
+++ utils/image2graph.py
@@ -200,7 +200,7 @@ if __name__ == "__main__":
 
     path_to_images = os.path.join(path_to_data, "images")
     imgs = [os.path.join(path_to_images, f"{i}.png") 
-            for i in range(100)]
+            for i in range(5000)]
     
     # imgs = ["data/images/13.png"]
 
