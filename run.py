1e726382d6e9b1ca07e9b0790dbf712a00358ddc
gauravsh0812 <gauravs@arizona.edu>
2023-12-04 14:53:12 -0700

    updating

--- run.py
+++ run.py
@@ -425,6 +425,6 @@ if __name__ == "__main__":
         world_size = len(gpus)
         os.environ["MASTER_ADDR"] = "localhost"
         os.environ["MASTER_PORT"] = "29890"
-        ddp_main(world_size, gpus)
+        ddp_main(world_size)
     else:
         train_model()
