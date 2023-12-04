918756e44953a3583d40c70e61d1fc597a925248
gauravsh0812 <gauravs@arizona.edu>
2023-12-04 15:31:39 -0700

    updating

--- run.py
+++ run.py
@@ -212,7 +212,7 @@ def train_model(rank=None,):
                 model.to(device),
                 device_ids=[rank],
                 output_device=rank,
-                find_unused_parameters=True,
+                # find_unused_parameters=True,
             )
         else:
             print(f"using gpu {str(gpus)}...")
