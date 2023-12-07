1b379b14a2cbabdeede4ae1376a5643d5c443cb2
gauravsh0812 <gauravs@arizona.edu>
2023-12-07 11:09:14 -0700

    updating

--- utils/bleu_score.py
+++ utils/bleu_score.py
@@ -1,10 +1,10 @@
 import os
 
 def calculate_bleu_score():
-    tt = open("logs/test_targets_100K.txt").readlines()
-    tp = open("logs/test_predicted_100K.txt").readlines()
-    _tt = open("logs/final_targets.txt", "w")
-    _tp = open("logs/final_preds.txt", "w")
+    tt = open("./logs/test_targets_100K.txt").readlines()
+    tp = open("./logs/test_predicted_100K.txt").readlines()
+    _tt = open("./logs/final_targets.txt", "w")
+    _tp = open("./logs/final_preds.txt", "w")
 
     for i, j in zip(tt, tp):
         eos_i = i.find("<eos>")
