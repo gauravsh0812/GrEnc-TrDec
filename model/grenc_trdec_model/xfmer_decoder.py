cb8b053c156780a8ec96fb724bfbbb03b9d919a7
gauravsh0812 <gauravs@arizona.edu>
2024-01-31 07:48:06 +0530

    updating

--- model/grenc_trdec_model/xfmer_decoder.py
+++ model/grenc_trdec_model/xfmer_decoder.py
@@ -97,7 +97,6 @@ class Transformer_Decoder(nn.Module):
         trg = trg[:-1, :]  # (max_len-1, B)
 
         sequence_length = trg.shape[0]
-        print("------------------: ", trg.is_cuda)
         trg_attn_mask = self.generate_square_subsequent_mask(
                                     sequence_length).to(self.device)  # (max_len-1, max_len-1)
 
