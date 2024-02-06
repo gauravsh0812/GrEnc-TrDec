cfaf3625a0b8d99fc2b9e426d3445f5b0635e5e0
gauravsh0812 <gauravs@arizona.edu>
2024-02-06 10:44:18 +0530

    updating

--- src/testing.py
+++ src/testing.py
@@ -66,6 +66,7 @@ def evaluate(
                     pred_arr = [
                         vocab.itos[ipred] for ipred in preds.int()[idx, :]
                     ]
+                    print("pred_arr: ", pred_arr)
                     pred_seq = " ".join(pred_arr)
                     pred_seqs.write(pred_seq + "\n")
 
